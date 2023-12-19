816797bac21f15184f340a65a2ad70d4337a48e3
RyaNGosling2 <prib-211_582519@volsu.ru>
2023-12-19 20:26:38 +0300

    im sorry

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace pmm3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(758, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 245);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series0";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(740, 413);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 456);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "100";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(759, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(29, 161);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace pmm3
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        double xd(double c,double t,double h)
+        {
+            return (c * c * t * t) / (h * h);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double[,] arry = new double[101, 2001];
+            double x, h = 0.01;
+            double tau = 0.002;
+            double t = 0, c = 1;
+            
+            for (int i = 0; i < 101; i++)
+            {
+                arry[i, 0] = 0;
+                arry[i, 1] = 0;
+            }
+            for (int i = 2; i < 2001; i++)
+            {
+                arry[0, i] = 0;
+                arry[100, i] = t * Math.Exp(-t);
+                t += tau;
+            }
+
+
+            for (int j = 3; j < 2001; j++)
+            {
+                for (int i = 1; i < 100; i++)
+                {
+                    //arry[i,j]=2*(1-xd(c,tau,h))*arry[i,j-1] - arry[i,j-2] + xd(c, tau, h)* (arry[i + 1, j - 1] + arry[i - 1, j - 1]);
+                    arry[i, j] = (arry[i + 1, j - 1] - 2 * arry[i, j - 1] + arry[i - 1, j - 1])*xd(c,tau,h) + 2 * arry[i, j - 1] - arry[i, j - 2];
+                }
+                //arry[100, j] = (2*arry[99, j - 1] - 2 * arry[100, j - 1]) * xd(c, tau, h) + 2 * arry[100, j - 1] - arry[100, j - 2];
+            }
+            x = 0.00;
+            int jg = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < 101; i++)
+            {
+                chart1.Series["Series0"].Points.AddXY(x, arry[i, jg]);
+                x += h;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Series0"].Points.Clear();
+        }
+    }
+}
